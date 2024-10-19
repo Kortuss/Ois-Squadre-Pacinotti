afa91336ca93929564e1e5b5678ca1a620a101df
Hiras Picano <128425402+Kortuss@users.noreply.github.com>
2024-10-19 23:40:41 +0200

    Update git.cpp

--- git.cpp
+++ git.cpp
@@ -1,5 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+std::map<string, bool> visited;
+std::map<string, vector<string>> adj;
+std::map<string, bool> adjEdit;
+vector<string> Output;
+string dfs(string i) {
+    bool k;
+    if (adjEdit.find(i) == adjEdit.end())
+        k = true;
+    else 
+        k = adjEdit[i];
+    string localOut = "";
+    for (auto u : adj[i]) {
+        string a = dfs(u);
+        localOut += a;
+        if (a == "")
+            k = false;
+    }
+    //cout << k << " " << i << endl;
+    if (k)
+        return i;
+    cout << localOut;
+    return "";
+}
 vector<string> tokenize (string in){
     
     vector<string> tokens;
@@ -26,7 +49,7 @@ int main()
     //std::map<string, int> mappa;
     //std::vector<vector<int>> adj;
     std::map<string, bool> adjTruth;
-    std::map<string, vector<string>> adj;
+
     for (int i = 0; i < N; i++){
         bool edited;
         cin >> edited;
@@ -35,6 +58,8 @@ int main()
         vector<string> Tokens(tokenize(in));
         std::reverse(Tokens.begin(), Tokens.end());
         string prec = Tokens[0];
+        cout << Tokens[0] << endl;
+        adjEdit[Tokens[0]] = edited;
         for (int i = 1; i < Tokens.size(); i++)
         {
             if (adjTruth[prec])
@@ -48,6 +73,7 @@ int main()
         adjTruth[prec] = true;
         adj["/"].push_back(prec);
     }
+    cout << dfs("/");
 
     return 0;
 }
